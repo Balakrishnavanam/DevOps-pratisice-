e8df2c22dc4649b6398989baa30f8b81ac3b8e17
Balakrishnavanam <vanambalu@gmail.com>
2022-07-22 20:16:18 +0530

    try file

---
+++ k.ppy.py
@@ -0,0 +1,21 @@
+a=int(input("enter the value of a,"))
+b=int(input("enter the value of b,"))
+c=int(input("enter the value of c,"))
+if((a<b)and(a<c)):
+  print('enter the greater value among three a')
+elif((b<c)and(b<a)):
+  print('enter the greater value among three b')
+else:
+  print('c')
+                            
+                
+
+
+
+
+
+
+
+
+                    
+                    
